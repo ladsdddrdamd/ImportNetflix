e27aa8d847eb9ecdc213c9e5beea0b82b59ebcc5
Liam Doherty <ladsdddrdamd@gmail.com>
2021-04-04 15:51:09 +0100

    Update for Jupiter & GIT Viewing

---
+++ ImportNetflix/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df= pd.read_csv(r"C:\UCD2021\PandasTesting\netflix_titles.csv")
+
+print(df.head(2))
+
+print(df.tail(2))
+
+print(df.shape)
+
+print(df.columns)
+
+df.isnull().sum
+
+
+
